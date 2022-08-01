eead654b0f7f732ae5bb128e068bb027ea766a8f
adiaber1994 <106983011+adiaber1994@users.noreply.github.com>
2022-08-01 21:46:32 +0300

    .

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let elem = document.querySelector('#root')
+
+let myInput = document.createElement("input");
+myInput.setAttribute("type", "Number")
+
+elem.appendChild(myInput);
+
+let myInput2 = document.createElement("input")
+myInput2.setAttribute("type", "Number");
+
+elem.appendChild(myInput2);
+
+let myInput3 = document.createElement("input")
+myInput3.setAttribute("type", "Number");
+
+elem.appendChild(myInput3);
